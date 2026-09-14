4882358b94b1ccfffc7a3bf329bff51b5f391814
Lalo Galvan <106835113+lalo-galvan@users.noreply.github.com>
2026-09-14 16:11:27 -0600

    Declare every Slack connection event PagerDuty stores

--- security.tf
+++ security.tf
@@ -65,20 +65,7 @@ resource "pagerduty_slack_connection" "security" {
   workspace_id      = var.slack_workspace_id
 
   config {
-    events = [
-      "incident.triggered",
-      "incident.acknowledged",
-      "incident.escalated",
-      "incident.resolved",
-      "incident.reassigned",
-      "incident.unacknowledged",
-      "incident.delegated",
-      "incident.priority_updated",
-      "incident.responder.added",
-      "incident.responder.replied",
-      "incident.status_update_published",
-      "incident.reopened"
-    ]
+    events     = local.slack_connection_events
     priorities = ["*"]
   }
 }
